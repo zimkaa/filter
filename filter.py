5e9e1e713c99f78f8a1054ff834ec1c15d83405b
Anton Zimin <zimka87@gmail.com>
2021-07-09 20:06:24 +0300

    init commit

---
+++ filter.py
@@ -0,0 +1,23 @@
+from confluent_kafka import Consumer
+
+
+c = Consumer({
+    'bootstrap.servers': 'localhost:9092',
+    'group.id': 'listener',
+    'auto.offset.reset': 'earliest',
+})
+
+c.subscribe(['registrations'])
+
+while True:
+    msg = c.poll(1.0)
+
+    if msg is None:
+        continue
+    if msg.error():
+        print("Consumer error: {}".format(msg.error()))
+        continue
+
+    print('Received message: {}'.format(msg.value().decode('utf-8')))
+
+c.close()
