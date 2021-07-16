efcb547323ed76874442aecdc6a4f94827e48856
Anton Zimin <zimka87@gmail.com>
2021-07-16 19:30:18 +0300

    my_filter

--- workflow/filter.py
+++ workflow/filter.py
@@ -1,33 +1,42 @@
+import json
+
 from confluent_kafka import Consumer
 from confluent_kafka import Producer
 
 import config
 
 
-producer = Producer({'bootstrap.servers': config.HOST})
+def main():
+    producer = Producer({'bootstrap.servers': config.HOST})
 
-consumer = Consumer({
-    'bootstrap.servers': config.HOST,
-    'group.id': 'listener_events',
-    'auto.offset.reset': 'earliest',
-})
+    consumer = Consumer({
+        'bootstrap.servers': config.HOST,
+        'group.id': 'listener_events',
+        'auto.offset.reset': 'earliest',
+    })
 
-consumer.subscribe(['events'])
+    consumer.subscribe(['events'])
 
-while True:
-    msg = consumer.poll(1.0)
+    while True:
+        msg = consumer.poll(1.0)
 
-    if msg is None:
-        continue
-    if msg.error():
-        print(f"Consumer error: {msg.error()}")
-        continue
+        if msg is None:
+            continue
+        if msg.error():
+            print(f"Consumer error: {msg.error()}")
+            continue
 
-    data = msg.value().decode("utf-8")
-    new_data = f"New {data}"
+        data = msg.value().decode("utf-8")
+        filtred_data_json = json.loads(data)
 
-    producer.produce('filtred', new_data.encode())
-    producer.flush()
-    print(f'Received message: {msg.value().decode("utf-8")}')
+        bitcoin = {'bitcoin': filtred_data_json['bitcoin']}
+        ethereum = {'ethereum': filtred_data_json['ethereum']}
 
-consumer.close()
+        producer.produce('filtred', json.dumps(ethereum).encode())
+        producer.produce('filtred', json.dumps(bitcoin).encode())
+        
+        producer.flush()
+
+
+if __name__ == "__main__":
+    main()
